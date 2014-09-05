c1837c8b80fb5a574927fb440b53e6da21ac999d
psweetpotato <p.sweetpotato@gmail.com>
2014-09-05 11:20:59 -0400

    adds single-user authentication for twitter

--- app.rb
+++ app.rb
@@ -35,7 +35,7 @@ class App < Sinatra::Base
   TWIT_CONSUMER_SECRET = "xycELG4QVvRPdP1dHMugeXUgmAmODE3CekS7VPD4Jk1MIhDYXf"
   TWIT_OWNER_ID = "2699174835"
   TWIT_ACCESS_TOKEN = "2699174835-zejsRftMeSZnTL6MGMW4PvbMoWvuHNh0UeUvgws"
-  TWIT_ACCESS_TOKEN_SECRET = "k2wzFh0LJy4zxaUPdPQhJ5mXkNrGug8jqBk7W2QgXv8CL"
+  TWIT_ACCESS_SECRET = "k2wzFh0LJy4zxaUPdPQhJ5mXkNrGug8jqBk7W2QgXv8CL"
   WU_KEY = "611266f891f71333"
   YORK_SEARCH_KEY = "89b8034ef450f0c931fa447d7dca0d8d:1:69766109"
 
@@ -46,6 +46,8 @@ class App < Sinatra::Base
   @client = Twitter::REST::Client.new do |config|
     config.consumer_key    = "TWIT_CONSUMER_KEY"
     config.consumer_secret = "TWIT_CONSUMER_SECRET"
+    config.access_token        = "TWIT_ACCESS_TOKEN"
+    config.access_token_secret = "TWIT_ACCESS_SECRET"
   end
 
   ########################
@@ -57,7 +59,7 @@ class App < Sinatra::Base
   end
 
   get('/profile') do
-    client.user("Psweetpotato")
+    @client.user("Psweetpotato")
     render(:erb, :profile)
   end
 
