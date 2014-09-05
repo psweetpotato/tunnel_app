abd0b7b9de3e08ced92c0ce8c6a2ecf1c609366b
psweetpotato <p.sweetpotato@gmail.com>
2014-09-05 11:23:39 -0400

    updates twitter client set up and initializes user

--- app.rb
+++ app.rb
@@ -44,12 +44,13 @@ class App < Sinatra::Base
   ########################
 
   @client = Twitter::REST::Client.new do |config|
-    config.consumer_key    = "TWIT_CONSUMER_KEY"
-    config.consumer_secret = "TWIT_CONSUMER_SECRET"
-    config.access_token        = "TWIT_ACCESS_TOKEN"
-    config.access_token_secret = "TWIT_ACCESS_SECRET"
+    config.consumer_key    = TWIT_CONSUMER_KEY
+    config.consumer_secret = TWIT_CONSUMER_SECRET
+    config.access_token        = TWIT_ACCESS_TOKEN
+    config.access_token_secret = TWIT_ACCESS_SECRET
   end
 
+binding.pry
   ########################
   # Routes
   ########################
